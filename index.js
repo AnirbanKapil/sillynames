497e239d99cd4cff9e5567b52aaa46aeb693d2c6
Anirban Kapil <anirbankapil@gmail.com>
2023-12-30 12:35:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// const sillyName = require("sillyname");
+import generateName from "sillyname";
+const myNam = generateName();
+console.log(`my sillyname is ${myNam}.`);
+
+
+
+import superheroes from "superheroes";
+const myNam2 = superheroes.random();
+
+console.log(`My superhero name is ${myNam2}`);
